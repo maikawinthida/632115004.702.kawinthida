3ddb2b0e8a435e7ac294a8f205f2dccb8669d2cc
maikawinthida <mai_ammi@hotmail.com>
2021-01-13 21:38:30 +0700

    3 hello

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world at  camt");
+        System.out.println("hello world at  camt,cmu");
     }
 }
